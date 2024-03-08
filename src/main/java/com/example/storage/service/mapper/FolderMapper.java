38a0b43f37ad98c839beb45cbd4cd80352b618d2
Jc Abalos <jc4balos@gmail.com>
2024-03-08 14:24:47 +0800

    created folderdtowith perms

--- src/main/java/com/example/storage/service/mapper/FolderMapper.java
+++ src/main/java/com/example/storage/service/mapper/FolderMapper.java
@@ -1,10 +1,14 @@
 package com.example.storage.service.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import com.example.storage.service.dto.FolderDto;
 import com.example.storage.service.dto.FolderDtoView;
 import com.example.storage.service.model.Folder;
+import com.example.storage.service.model.FolderAccess;
 
 import lombok.Data;
 
@@ -20,7 +24,11 @@ public class FolderMapper {
         folderDtoView.setFolderParentId(folder.getFolderParentId());
         folderDtoView.setCreatedDateTime(folder.getCreatedDateTime());
         folderDtoView.setUpdatedDateTime(folder.getUpdatedDateTime());
-        folderDtoView.setOwner(folder.getOwner());
+        folderDtoView.setOwnerId(folder.getOwner().getUserId());
+        folderDtoView.setOwnerFirstName(folder.getOwner().getFirstName());
+        folderDtoView.setOwnerMiddleName(folder.getOwner().getMiddleName());
+        folderDtoView.setOwnerLastName(folder.getOwner().getLastName());
+        folderDtoView.setOwnerUsername(folder.getOwner().getUserName());
         return folderDtoView;
     }
 
@@ -30,6 +38,31 @@ public class FolderMapper {
         folder.setFolderDescription(folderDto.getFolderDescription());
         folder.setFolderParentId(folderDto.getFolderParentId());
         return folder;
+    }
+
+    public List<FolderDtoView> toFolderDtoWithPerms(List<FolderAccess> folderList) {
+        List<FolderDtoView> foldersListDtoView = new ArrayList<>();
+        for (FolderAccess folder : folderList) {
+            FolderDtoView folderDtoView = new FolderDtoView();
+            folderDtoView.setFolderId(folder.getFolder().getFolderId());
+            folderDtoView.setFolderName(folder.getFolder().getFolderName());
+            folderDtoView.setFolderDescription(folder.getFolder().getFolderDescription());
+            folderDtoView.setFolderParentId(folder.getFolder().getFolderParentId());
+            folderDtoView.setCreatedDateTime(folder.getFolder().getCreatedDateTime());
+            folderDtoView.setUpdatedDateTime(folder.getFolder().getUpdatedDateTime());
+            folderDtoView.setOwnerId(folder.getFolder().getOwner().getUserId());
+            folderDtoView.setOwnerFirstName(folder.getFolder().getOwner().getFirstName());
+            folderDtoView.setOwnerMiddleName(folder.getFolder().getOwner().getMiddleName());
+            folderDtoView.setOwnerLastName(folder.getFolder().getOwner().getLastName());
+            folderDtoView.setOwnerUsername(folder.getFolder().getOwner().getUserName());
+            folderDtoView.setAllowAddFolder(folder.getAllowAddFolder());
+            folderDtoView.setAllowModifyFolder(folder.getAllowModifyFolder());
+            folderDtoView.setAllowDeleteFolder(folder.getAllowDeleteFolder());
+            folderDtoView.setAllowViewFolder(folder.getAllowViewFolder());
+            foldersListDtoView.add(folderDtoView);
+        }
+        return foldersListDtoView;
+
     }
 
 }
